215ee60e8a8774d401b847b616fca89924522fdf
Jérôme Wiedemann <jerome@wnetworks.org>
2023-04-02 16:36:25 +0000

    chore: new CI

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import {
-  terser
-} from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 import serve from 'rollup-plugin-serve';
 import json from '@rollup/plugin-json';
 
+// eslint-disable-next-line no-undef
 const dev = process.env.ROLLUP_WATCH;
 
 const serveopts = {
@@ -27,17 +26,39 @@ const plugins = [
   json(),
   babel({
     exclude: 'node_modules/**',
+    babelHelpers: 'bundled',
+    babelrc: false,
+    presets: [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry',
+        targets: '> 0.25%, not dead',
+      },
+    ],
   }),
   dev && serve(serveopts),
-  !dev && terser(),
+  !dev &&
+    terser({
+      // format: {
+      //   comments: false,
+      // },
+      mangle: {
+        safari10: true,
+      },
+    }),
 ];
 
-export default [{
-  input: 'src/decluttering-card.ts',
-  output: {
-    dir: './dist',
-    format: 'es',
-    sourcemap: dev ? true : false,
+export default [
+  {
+    input: 'src/decluttering-card.ts',
+    output: {
+      dir: './dist',
+      format: 'es',
+      sourcemap: dev ? true : false,
+    },
+    plugins: [...plugins],
+    watch: {
+      exclude: 'node_modules/**',
+    },
   },
-  plugins: [...plugins],
-}, ];
+];
